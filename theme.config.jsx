1fb22973df326ba6090eb1e38d73be62d7788df6
Sal Becker <physicsfu@gmail.com>
2026-01-01 23:34:49 -0500

    Initial Nextra setup with character tools

---
+++ theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+  logo: <span>URS - RPG Guide</span>,
+  project: {
+    link: 'https://github.com/yourusername/urs'
+  },
+  docsRepositoryBase: 'https://github.com/yourusername/urs',
+  footer: {
+    text: 'URS Roleplaying Game Guide'
+  }
+}
